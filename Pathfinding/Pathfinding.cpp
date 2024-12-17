e5ac03b6563faf39e99f4eb31bf56da6490d9c20
FloWidmann <flo.widmann07@outlook.com2>
2024-12-17 15:59:14 +0100

    Projektdateien hinzufügen.

---
+++ Pathfinding/Pathfinding.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <raylib.h>
+
+
+enum Colors
+{
+
+};
+struct Field
+{
+	int iPosx;
+	int iPosy;
+	void draw_field()
+	{
+
+	}
+};
+
+
+
+
+
+
+
+int main()
+{	
+	const int iScreenHeight = 736;
+	const int iScreenWidth = 1280;
+	const int iPixelSize = 32;
+	InitWindow(iScreenWidth, iScreenHeight, "Pathfinding");
+	
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		for (int i = 0; i < 1280 ; i += iPixelSize)
+		{
+			for (int j = 0; j < 720 ; j += iPixelSize)
+			{
+				DrawRectangle(i, j, iPixelSize, iPixelSize, GREEN);
+				DrawLine(i, 0, i, iScreenHeight, BLACK);
+				DrawLine(0, j, iScreenWidth, j, BLACK);
+
+			}
+		}
+
+		EndDrawing();
+	}
+
+	return 0;
+}
